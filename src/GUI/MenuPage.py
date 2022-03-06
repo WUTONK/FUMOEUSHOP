50d448cc72ac015078efd4a1ac3a6de9d1831d4b
WUTONK <wjt474696120@gmail.com>
2022-03-06 13:08:54 +0800

    null

---
+++ src/GUI/MenuPage.py
@@ -0,0 +1,49 @@
+"""
+菜单页面及菜单栏页面
+"""
+
+import re
+import json
+import PyQt6
+
+def main():
+    pass
+
+class UpperMenuBar:
+    """
+    上方菜单栏页面
+    """
+
+    def Avatar():
+        """
+        负责玩家属性，例如头像
+        """
+        
+
+        pass
+
+    def NameColumn():
+        """
+        玩家姓名栏
+        """
+
+        pass
+
+    def moneyColumn():
+        """金钱栏"""
+        pass
+
+    def moneyColumnGroove():
+        """玩家点击金钱栏后的反馈"""
+        pass
+        
+    def NameColumnGroove():
+        """玩家点击姓名栏后的反馈"""
+    
+    def AvatarGroove():
+        """玩家点击头像后的反馈"""
+        print("调试信息：头像被点击")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/GUI/StatisticsPage.py
@@ -0,0 +1,22 @@
+"""
+统计数据页面
+"""
+
+from msilib.schema import Class
+
+
+class StatisticsPage_money:
+    """
+    统计页面金钱一项
+    """
+    pass
+class StatisticsPage_:
+    """
+    统计页面销售一项
+    """
+
+class StatisticsPage_:
+    """
+    统计页面通用统计数据一项
+    """
+    
