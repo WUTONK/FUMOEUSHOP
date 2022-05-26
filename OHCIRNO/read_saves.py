ba4d71a60d2de25d2a3b0cf76f68d54a5e21cd3c
WUZITONG <wjt474696120@gmail.com>
2022-05-26 17:12:20 +0800

    更新README，文件目录

--- OHCIRNO/read_saves.py
+++ OHCIRNO/read_saves.py
@@ -2,16 +2,18 @@
 负责读取存档功能
 """
 from OHCIRNO import default_config
+from default_config import *
 
 class file_define():
     def __init__(self):
-        save_file = default_config.SAVE_FILE
-        csv_file = default_config.SAVE_FILE + default_config.CSV_FUMO_LIST_NAME
-        config_file = default_config.SAVE_FILE + default_config.CSV_CONFIG
+        save_file = ROOT_DIR+SAVE_PATH
+        csv_file = SAVE_FILE + CSV_FUMO_NAME_LIST
+        config_file = SAVE_FILE + 
         
 
 class csv_read():
 
     class read_fumo_list_name_csv():
         pass
-    class read_
+    class read_Statistics_data():
+        pass
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # FUMOEUSHOP
-一个阴间小游戏
+
+东方同人小游戏，基于electron(前端)+djando(后端)，计划支持linux+macos+windows，移动端计划使用Flutter实现
+
+本项目现在仍属于非常初级的开发阶段，以下是TODO列表
+
+## 环境
+[] 配置完成Djando环境
+[] 配置完成electron环境
+## DEMO
